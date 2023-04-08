cb9d2387d65048b32577c55d3f40130d8b3632a2
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-04-08 11:54:47 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -3,9 +3,10 @@ function init(){
     const fileReader = new FileReader()
 
     fileReader.onloadend = () => {
-        let base64 = fileReader.result 
-        console.log(input.files[0])
-        console.log(base64)
+        let base64 = fileReader.result.replace(
+            /^data:image\/(png|jpeg|jpg);base64,/, ''
+        )
+        
     }
 
     input.addEventListener('change', () => {
